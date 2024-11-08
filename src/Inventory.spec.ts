94eb7042683847b27f1bb152e6d2489f1293e1a9
Alberto González <albertogonzalez@acidtango.com>
2024-11-08 15:42:50 +0000

    refactor: test refactor

--- src/Inventory.spec.ts
+++ src/Inventory.spec.ts
@@ -1,14 +1,19 @@
-import { describe, expect, it } from "vitest"
+import { beforeEach, describe, expect, it } from "vitest"
 import { Inventory } from "./Inventory.js"
 
 describe("Inventory", () => {
+  let inventory: Inventory
+
+  beforeEach(() => {
+    inventory = new Inventory()
+  })
+
   it("can be created", () => {
-    expect(new Inventory()).not.toBeUndefined()
+    expect(inventory).not.toBeUndefined()
   })
 
   it("is empty", () => {
     const now = new Date()
-    const inventory = new Inventory()
 
     expect(inventory.project(now)).toEqual([])
   })
