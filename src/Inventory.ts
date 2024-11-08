12d231b697aa5a7a8bcbbdda390a381b36352ab3
Alberto González <albertogonzalez@acidtango.com>
2024-11-08 15:59:00 +0000

    refactor: datekey and dateExists

--- src/Inventory.ts
+++ src/Inventory.ts
@@ -4,11 +4,16 @@ export class Inventory {
   constructor(private items: Record<string, Fruit[]> = {}) {}
 
   add(date: Date, element: Fruit) {
-    if (this.items[date.toLocaleDateString()]) this.items[date.toLocaleDateString()].push(element)
-    else this.items[date.toLocaleDateString()] = [element]
+    const dateKey = date.toLocaleDateString()
+    const dateKeyExists = Boolean(this.items[dateKey])
+
+    if (dateKeyExists) this.items[dateKey].push(element)
+    else this.items[dateKey] = [element]
   }
 
   project(date: Date) {
-    return this.items[date.toLocaleDateString()] ?? []
+    const dateKey = date.toLocaleDateString()
+
+    return this.items[dateKey] ?? []
   }
 }
